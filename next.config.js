dcade885c0c5baf339efc10e77d911cf509fd6ac
samin.karki1 <samin.karki@appliflo.com>
2022-09-23 17:14:48 +0545

    config updated

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images:{
-    domains:['www.example.com']
-  }
-}
+  images: {
+    domains: ['www.example.com'],
+  },
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
