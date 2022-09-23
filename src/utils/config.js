1e00fec90c499005e70e7c47d77aaa40d58a51cb
samin.karki1 <samin.karki@appliflo.com>
2022-09-23 16:21:07 +0545

    multiple prediction config added

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,25 @@
 const envConfig = {
-  baseUrl: process.env.BASE_URL || '',
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || '',
   predictPaths: {
     basePath: '/predict/',
     get handwrittenDigits() {
-      console.log(this);
       return this.basePath + 'digit';
     },
+    get catDogPrediction() {
+      return this.basePath + 'cat-dog';
+    },
   },
+  classificationOptions: [
+    {
+      id: 1,
+      label: 'Handwritten digits classification',
+    },
+    {
+      id: 2,
+      label: 'Cat or Dog Prediction',
+      description: 'This is used to determine whether an input image is a dog or a cat.'
+    },
+  ],
 };
 Object.seal(envConfig);
-
 export default envConfig;
